916a7d55fe4b67144c2d83059df497e6f9e61c2c
biancasilva <bianca@fanhero.com>
2022-08-09 10:45:38 -0300

    feat: add providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css"
+import { AuthProvider } from "./contexts/AuthContext"
+import Router from "routes/Router"
+import { BrowserRouter } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react"
 
 function App() {
-	return <div className='App'>teste</div>
+	return (
+		<AuthProvider>
+			<BrowserRouter>
+				<ChakraProvider>
+					<Router />
+				</ChakraProvider>
+			</BrowserRouter>
+		</AuthProvider>
+	)
 }
 
 export default App
